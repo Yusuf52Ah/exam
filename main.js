e5c34238637d1a151428e174be65cad56e131256
Yusuf52Ah <yusufxon5225@gmail.com>
2025-08-07 17:05:13 +0500

    Exam

---
+++ main.js
@@ -0,0 +1,98 @@
+// 1 chi masala
+
+// function makeNegative(num) {
+//     if (num > 0) {
+//         console.log(-num);
+        
+//     } else{
+//         console.log(num);
+//     }
+    
+// }
+
+// makeNegative(5)
+// makeNegative(-3)
+// makeNegative(0)
+
+// 2 chi masala
+
+// function evenOrOdd(num) {
+//     if (num % 2 === 0) {
+//         console.log("Even"); 
+//     } else {
+//         console.log("Odd");
+        
+//     }
+
+// }
+
+// evenOrOdd(2)
+
+// 3 chi masala
+
+// function removeChar(str) {
+//     let arr = str.split("")
+//     let res = arr.pop()
+//     let res2 = arr.shift()
+//     let res3 = arr.join("")
+//     console.log(res3);
+    
+// }
+
+// removeChar("Hello")
+
+// 4 chi masala
+
+// function solution(str) {
+//     let arr = str.split("")
+//     let res = arr.reverse()
+//     let res2 = arr.join("")
+//     console.log(res2);
+    
+// }
+
+// solution("World")
+
+// 5 chi masala
+
+// function positiveSum(arr) {
+//     let arr1 = arr.filter((n) => n > 0)
+//     let res = arr1.reduce((acc, cur) => acc + cur)
+//     console.log(res);
+// }
+
+// positiveSum([1, -2, 3, 4, -5])
+
+// 6 chi masala
+
+// 7 chi masala
+
+// function getMiddle(s){
+// if (s.length() % 2 == 1 ) {
+//   let res = s.slice(s.length()/2,s.length()/2+1)
+//   console.log(res);
+  
+    
+// } else {
+// console.log(s.slice(1,3));
+ 
+// }
+
+// }
+// getMiddle("test")
+// getMiddle("testing")
+
+// 8 chi masala
+
+// function findShort(s){
+//  let arr = s.split(" ");
+//  let res = arr[1].length;
+// for (let i = 1; i < arr.length; i++) {
+//  if (arr[i].length < res) {
+//  res = arr[i].length; }
+//  console.log(res);
+            
+// }
+// }
+// findShort(" I Love Javascript Frontend")
+// findShort("Simple and easy tasks")
